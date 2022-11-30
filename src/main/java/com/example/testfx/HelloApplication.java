570a79e0ad53f9a9481155a8c3353747bee97d70
amna <antoniomiguel2001@hotmail.es>
2022-11-30 22:09:37 +0100

    fino

--- src/main/java/com/example/testfx/HelloApplication.java
+++ src/main/java/com/example/testfx/HelloApplication.java
@@ -17,25 +17,28 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         TableView<Alumno> tablaAlumnos = new TableView<Alumno>();
         tablaAlumnos.setEditable(true);
-        TableColumn<Alumno,String>columnaNombre = new TableColumn<Alumno, String>("Nombre");
+        TableColumn<Alumno, String> columnaNombre = new TableColumn<Alumno, String>("Nombre");
         columnaNombre.setMinWidth(300);
         //Para que en cada celda aparezca el atributo nombre de la clase Alumno
-        columnaNombre.setCellValueFactory(new PropertyValueFactory<Alumno,String>("nombre"));
+        columnaNombre.setCellValueFactory(new PropertyValueFactory<Alumno, String>("nombre"));
 
-        TableColumn<Alumno,String>columnaApellidos = new TableColumn<Alumno, String>("Apellidos");
-        columnaNombre.setMinWidth(300);
-        columnaNombre.setCellValueFactory(new PropertyValueFactory<Alumno,String>("apellidos"));
+        TableColumn<Alumno, String> columnaApellidos = new TableColumn<Alumno, String>("Apellidos");
+        columnaApellidos.setMinWidth(300);
+        columnaApellidos.setCellValueFactory(new PropertyValueFactory<Alumno, String>("apellidos"));
 
-        TableColumn<Alumno,String>columnaEdad = new TableColumn<Alumno, String>("Edad");
-        columnaNombre.setMinWidth(300);
-        columnaNombre.setCellValueFactory(new PropertyValueFactory<Alumno,String>("edad"));
+        TableColumn<Alumno, String> columnaEdad = new TableColumn<Alumno, String>("Edad");
+        columnaEdad.setMinWidth(300);
+        columnaEdad.setCellValueFactory(new PropertyValueFactory<Alumno, String>("edad"));
         //Añade las columnas al TableView
         tablaAlumnos.getColumns().addAll(columnaNombre,columnaApellidos,columnaEdad);
 
         ObservableList<Alumno> lista = FXCollections.observableArrayList();
-        lista.add(new Alumno());
-        lista.add(new Alumno());
-        lista.add(new Alumno());
+        lista.add(new Alumno("Nombre 1", "Ape 1", 13));
+        lista.add(new Alumno("Nombre 2", "Ape 2", 15));
+        lista.add(new Alumno("Nombre 3", "Ape 3", 16));
+        columnaNombre.setCellValueFactory(new PropertyValueFactory<Alumno,String>("nombre"));
+        columnaApellidos.setCellValueFactory(new PropertyValueFactory<Alumno,String>("apellidos"));
+        columnaEdad.setCellValueFactory(new PropertyValueFactory<Alumno,String>("edad"));
         tablaAlumnos.setItems(lista);
 
         VBox panelPrincipal = new VBox();
@@ -43,24 +46,11 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(panelPrincipal);
         stage.setScene(scene);
         stage.show();
-//        Button bt1 = new Button("OK");
-//        Label etiqueta1 = new Label("Nombre: ");
-//        TextField tf1 = new TextField();
-//
-//        VBox paneVertical  =new VBox();
-//        HBox h1 = new HBox();
-//
-//        h1.getChildren().addAll(etiqueta1,tf1);
-//        paneVertical.getChildren().addAll(bt1,h1);
-//
-//        Scene scene = new Scene(paneVertical,400,400);
-//        stage.setScene(scene);
-//        stage.show();
-////        bt1.setOnAction(e->printConsola(tf1.getText()));
 
     }
-    public void printConsola(String cad){
-        System.out.println("Hola "+cad);
+
+    public void printConsola(String cad) {
+        System.out.println("Hola " + cad);
     }
 
     public static void main(String[] args) {
